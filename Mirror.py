abca7ef5d9f7170355f42f5de367ee457dc94883
Eli Kastelein <eli.kastelein@gmail.com>
2015-12-17 12:52:30 -0800

    removed globals

--- Mirror.py
+++ Mirror.py
@@ -4,13 +4,11 @@ import imaplib
 import email
 import datetime
 import Tkinter
-
 from googlefinance import getQuotes
 
-the_temp, the_time, the_date, the_forecast, the_count, the_email_subject, = '', '', '', '', '', '',
-yesterday_closing = None
 FONT = 60
 FONT_2 = 45
+FONT_3 = 30
 
 with open("parameters.txt", "r") as text_file:
     parameters = text_file.read().splitlines()
@@ -25,9 +23,9 @@ STOCK = parameters[5]
 
 
 class Main(object):
-    """Builds a Tkinter object and fills its grid with 5 strings with the
+    """ Builds a Tkinter object and fills its grid with 5 strings with the
     methods: create_email_display, create_count_up, create_meteorology,
-    create_finances and create_date_time"""
+    create_finances and create_date_time """
     def __init__(self, master):
         self.master = master
         self.mainframe = Tkinter.Frame(self.master, bg='black')
@@ -35,6 +33,11 @@ class Main(object):
         self.build_grid()
 
         self.the_finances = None
+        self.the_date_time = None
+        self.yesterday_closing = 13.04
+        self.the_forecast = None
+        self.the_count = None
+        self.the_email_subject = None
 
         self.create_email_display()
         self.create_count_up()
@@ -43,27 +46,30 @@ class Main(object):
         self.create_date_time()
 
     def build_grid(self):
+        """ Builds a 2 column x 3 row grid for the display"""
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.rowconfigure(0, weight=0)
         self.mainframe.rowconfigure(1, weight=1)
         self.mainframe.rowconfigure(2, weight=0)
 
     def create_email_display(self):
-        self.display_email_subject = Tkinter.Label(self.mainframe, text=the_email_subject)
+        """ Creates a Tkinter label where email data can be displayed """
+        self.display_email_subject = Tkinter.Label(self.mainframe)
         self.display_email_subject.grid(row=0, column=0, sticky='nw')
 
         def change_the_emails():
-            global the_email_subject
+            """ Fetcheces """
+
             mail = initialize_mail_account()
             rv, data = mail.select("INBOX")
             if rv == 'OK':
                 latest_email = process_mailbox(mail)
                 mail.close()
 
-            if the_email_subject != latest_email:
-                the_email_subject = latest_email
+            if self.the_email_subject != latest_email:
+                self.the_email_subject = latest_email
                 self.display_email_subject.config(
-                    text= the_email_subject,
+                    text=self.the_email_subject,
                     font=("Helvetica", FONT_2),
                     bg='black',
                     fg='white',
@@ -77,7 +83,6 @@ class Main(object):
         self.display_count_up.grid(row=1, column=0,sticky='e')
 
         def change_the_count():
-            global the_count
             today = datetime.date.today()
 
             sig_year = int(SIGNIFICANT_DATE[0:4])
@@ -85,9 +90,9 @@ class Main(object):
             sig_day = int(SIGNIFICANT_DATE[8:10])
             count_today = (today - datetime.date(sig_year, sig_month, sig_day)).days
 
-            if the_count != count_today:
-                the_count = count_today
-                count_string = "{0} days with Sarah".format(the_count)
+            if self.the_count != count_today:
+                self.the_count = count_today
+                count_string = "{0} days with Sarah".format(self.the_count)
                 self.display_count_up.config(
                     text=count_string,
                     font=("Helvetica", FONT),
@@ -102,23 +107,23 @@ class Main(object):
         self.display_forecast = Tkinter.Label(self.mainframe)
         self.display_forecast.grid(row=2, column=0, sticky='es')
 
-        def change_forecast_value():
-            global the_forecast
+        def change_forecast_value(font=FONT_3):
             forecast = forecastio.load_forecast(FORECAST_IO_API, LATITUDE, LONGITUDE)
 
             now = forecast.currently()
             now_forecast = now.summary
             now_temp = now.temperature
+
             later = forecast.hourly()
             later_forecast = later.summary
 
-            now_later = "{0}C {1} \n {2}".format(int(now_temp), now_forecast, later_forecast)
+            now_later_string = "{0}C {1} \n {2}".format(int(now_temp), now_forecast, later_forecast)
 
-            if the_forecast != now_later:
-                the_forecast = now_later
+            if self.the_forecast != now_later_string:
+                self.the_forecast = now_later_string
                 self.display_forecast.config(
-                    text=now_later,
-                    font=("Helvetica", FONT_2),
+                    text=now_later_string,
+                    font=("Helvetica", font),
                     bg='black',
                     fg='white',
                     justify='right',
@@ -131,18 +136,14 @@ class Main(object):
         self.display_finances.grid(row=1, column=0, sticky='w')
 
         def change_finance_values():
-            #global the_finances
-            global yesterday_closing
-
             current_price = float(getQuotes('MUTF_CA:INI220')[0]['LastTradePrice'])
 
-            print datetime.datetime.now().strftime('%H')
             if datetime.datetime.now().strftime('%H') == 00:
                 print "Time is 00"
-                yesterday_closing = current_price
+                self.yesterday_closing = current_price
 
-            if yesterday_closing:
-                day_over_day = current_price - yesterday_closing
+            if self.yesterday_closing:  # could be initialized to None
+                day_over_day = current_price - self.yesterday_closing
                 if day_over_day >= 0:
                     display_string = "${0} (+{1})".format(current_price, day_over_day)
                 else:
@@ -168,19 +169,18 @@ class Main(object):
         self.display_date_time.grid(row=0, column=0, sticky='ne')
 
         def change_time_value():
-            global the_time
-            new_time = time.strftime('%I:%M:%S \n %b %d, %Y')
-            if new_time != the_time:
-                the_time = new_time
+            new_date_time = time.strftime('%I:%M:%S \n %b %d, %Y')
+            if self.the_date_time != new_date_time:
+                self.the_date_time = new_date_time
                 self.display_date_time.config(
-                    text=the_time,
+                    text=self.the_date_time,
                     font=("Helvetica", FONT),
                     bg='black',
                     fg='white',
                     justify='right'
                 )
 
-            self.display_date_time.after(999, change_time_value)
+            self.display_date_time.after(1000, change_time_value)
         change_time_value()
 
 
